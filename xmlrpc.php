75622c76f80689503c0c718028449030b454b67a
datailmiah <135482814+datailmiah@users.noreply.github.com>
2023-12-14 17:24:04 +0700

    Add files via upload

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://undangan-digital.net</homePageLink>
+		<homePageLink>https://weddingkita.net/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://undangan-digital.net/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://undangan-digital.net/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://undangan-digital.net/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://undangan-digital.net/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://undangan-digital.net/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://weddingkita.net/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://weddingkita.net/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://weddingkita.net/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://weddingkita.net/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://weddingkita.net/wp-json/" />
 			</apis>
 	</service>
 </rsd>
